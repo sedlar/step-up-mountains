e8e8871af94630b6b9b47b7a57ef2e78fb9cdae4
Skokan <sedlar@mail.muni.cz>
2015-02-06 23:08:41 +0100

    Updated admin view

--- stepupmountains/admin.py
+++ stepupmountains/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from stepupmountains.models import Mountain
+
+class MountainAdmin(admin.ModelAdmin):
+	fields = ['mountain_name', 'mountain_elevation', 'mountain_comment']
 
 # Register your models here.
+admin.site.register(Mountain, MountainAdmin)
