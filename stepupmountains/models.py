775a525b14ae3f3daa70ed4daec6d7027ccf4118
Skokan <sedlar@mail.muni.cz>
2015-02-06 22:36:45 +0100

    Added new model and migrated db

--- stepupmountains/models.py
+++ stepupmountains/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Mountain(models.Model):
+	mountain_name = models.CharField(max_length=200)
+	mountain_elevation = models.IntegerField()
+	comment = models.CharField(max_length=1000)
