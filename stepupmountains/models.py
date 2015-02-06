45ac715c54bca9665f0e8a21539f144402627a50
Skokan <sedlar@mail.muni.cz>
2015-02-06 22:54:26 +0100

    Updated db

--- stepupmountains/models.py
+++ stepupmountains/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Mountain(models.Model):
+class Mountains(models.Model):
 	mountain_name = models.CharField(max_length=200)
 	mountain_elevation = models.IntegerField()
 	comment = models.CharField(max_length=1000)
+	
+	def __str__(self):
+		return self.mountain_name + ' (' + str(self.mountain_elevation) + ' m.n.m.)'
