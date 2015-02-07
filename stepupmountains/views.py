f072105fdde6be495f6a2cf4c634984ff5435de6
Skokan <sedlar@mail.muni.cz>
2015-02-07 12:38:04 +0100

    Created view mountains view with template

--- stepupmountains/views.py
+++ stepupmountains/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from stepupmountains.models import Mountain
 
 # Create your views here.
 
 def mountain_list(request):
-	return HttpResponse("Hello world!!!")
+	all_mountains = Mountain.objects.order_by('-mountain_elevation');
+	context = {'mountain_list': all_mountains}
+	return render(request, 'stepupmountains/mountain_list.html', context)
