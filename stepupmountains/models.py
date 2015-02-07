7153db60166cce612dc3841e879dc977fd37da0b
Skokan <sedlar@mail.muni.cz>
2015-02-07 19:39:54 +0100

    Updated models to include users

--- stepupmountains/models.py
+++ stepupmountains/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Mountain(models.Model):
@@ -8,3 +9,12 @@ class Mountain(models.Model):
 	
 	def __str__(self):
 		return self.mountain_name + ' (' + str(self.mountain_elevation) + ' m.n.m.)'
+
+class Climbed(models.Model):
+	user = models.ForeignKey(User)
+	climbed_meters = models.IntegerField()
+
+class ClimbObject(models.Model):
+	user = models.ForeignKey(User)
+	object_name = models.CharField(max_length=200)
+	height = models.IntegerField()
