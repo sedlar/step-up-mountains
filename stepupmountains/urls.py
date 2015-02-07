e597d19247807ed04a4c57fd185245c0d9d07929
Skokan <sedlar@mail.muni.cz>
2015-02-07 22:29:02 +0100

    forgetten files for new view climb/

--- stepupmountains/urls.py
+++ stepupmountains/urls.py
@@ -3,3 +3,4 @@ from django.conf.urls import patterns, url
 from stepupmountains import views
 
 urlpatterns = patterns('', url(r'^$', views.mountain_list, name='mountain_list'),)
+urlpatterns = patterns('', url(r'^climb/$', views.climb_object, name='climb_object'),)
