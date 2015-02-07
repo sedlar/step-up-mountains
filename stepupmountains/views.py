ed90f5780c64b92a67d9720bfde66298b51500ac
Skokan <sedlar@mail.muni.cz>
2015-02-07 22:28:21 +0100

    Added new view climb/

--- stepupmountains/views.py
+++ stepupmountains/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from stepupmountains.models import Mountain
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -8,3 +9,14 @@ def mountain_list(request):
 	all_mountains = Mountain.objects.order_by('-mountain_elevation');
 	context = {'mountain_list': all_mountains}
 	return render(request, 'stepupmountains/mountain_list.html', context)
+
+def climb_object(request):
+	return HttpResponse("blah")
+	#climbed_object = get_object_or_404(request.POST['climbed_object']
+	#if request.user != climbed_object.user
+		#HttpResponseForbidden()
+	#climb = Climbed(climbed_height=climbed_object.height)
+	#climb.save()
+	#return HttpResponseRedirect(reverse('stepupmountains:mountain_list')
+
+		
