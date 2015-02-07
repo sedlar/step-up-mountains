978c91cbab9c0aa1c72d2294010ac31c5c1405cc
Skokan <sedlar@mail.muni.cz>
2015-02-07 11:21:50 +0100

    Updated admin view

--- stepupmountains/admin.py
+++ stepupmountains/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from stepupmountains.models import Mountain
 
 class MountainAdmin(admin.ModelAdmin):
+	list_display = ['mountain_name', 'mountain_elevation', 'mountain_comment']
 	fields = ['mountain_name', 'mountain_elevation', 'mountain_comment']
+	search_fields = ['mountain_name', 'mountain_elevation', 'mountain_comment']
+	ordering = ['-mountain_elevation', 'mountain_name']
 
 # Register your models here.
 admin.site.register(Mountain, MountainAdmin)
