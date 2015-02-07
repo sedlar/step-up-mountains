fbe3eedbeb9f69477be21f25e69b67f97222ea73
Skokan <sedlar@mail.muni.cz>
2015-02-07 23:31:04 +0100

    Added climbing buttons and navigation to dummy climb page

--- stepupmountains/urls.py
+++ stepupmountains/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls import patterns, url
 
 from stepupmountains import views
 
-urlpatterns = patterns('', url(r'^$', views.mountain_list, name='mountain_list'),)
-urlpatterns = patterns('', url(r'^climb/$', views.climb_object, name='climb_object'),)
+urlpatterns = patterns('', url(r'climb/$', views.climb_object, name='climb_object'),url(r'view/$', views.mountain_list, name='mountain_list'), )
--- stepupmountains/views.py
+++ stepupmountains/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from stepupmountains.models import Mountain
+from stepupmountains.models import ClimbingObject
 from django.contrib.auth.models import User
 
 # Create your views here.
 
 def mountain_list(request):
 	all_mountains = Mountain.objects.order_by('-mountain_elevation');
-	context = {'mountain_list': all_mountains}
+	all_objects = ClimbingObject.objects.all();
+	context = {'mountain_list': all_mountains, 'object_list': all_objects}
 	return render(request, 'stepupmountains/mountain_list.html', context)
 
 def climb_object(request):
