f1e7abfd5b16eab88d9439605e88ae8cbaa14fb4
Skokan <sedlar@mail.muni.cz>
2015-02-08 10:14:11 +0100

    Updated urls

--- myproject/urls.py
+++ myproject/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^stepupmountains/', include('stepupmountains.urls', namespace="stepupmountains")),
+    url(r'^.{0}', include('stepupmountains.urls', namespace="stepupmountains")),
 )
--- stepupmountains/urls.py
+++ stepupmountains/urls.py
@@ -2,4 +2,8 @@ from django.conf.urls import patterns, url
 
 from stepupmountains import views
 
-urlpatterns = patterns('', url(r'climb/$', views.climb_object, name='climb_object'),url(r'view/$', views.mountain_list, name='mountain_list'), )
+urlpatterns = patterns(
+	'',
+	url(r'climb/$', views.climb_object, name='climb_object'),
+	url(r'.*$', views.mountain_list, name='mountain_list'),
+	)
