a9a988862a639ec5b1f468d978baf1d1f15d3495
Skokan <sedlar@mail.muni.cz>
2015-02-08 10:19:11 +0100

    Added datetime field to Climb object

--- stepupmountains/models.py
+++ stepupmountains/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 class Mountain(models.Model):
@@ -20,4 +21,5 @@ class ClimbingObject(models.Model):
 class Climbed(models.Model):
 	user = models.ForeignKey(User)
 	climbed_object = models.ForeignKey(ClimbingObject)
+	datetime = models.DateTimeField(default=timezone.now)
 	
