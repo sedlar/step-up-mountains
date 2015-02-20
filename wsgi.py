1ad25205568400b261fc22d26a0909ca073541c5
Skokan <sedlar@mail.muni.cz>
2015-02-20 20:35:17 +0100

    Adding openshift files

---
+++ wsgi.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import os
+
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
