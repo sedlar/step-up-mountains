ed536e19698be185af558d9d707f57211e424465
Skokan <sedlar@mail.muni.cz>
2015-02-22 20:56:38 +0100

    Update of settings to use POSTGRESQL

--- myproject/settings.py
+++ myproject/settings.py
@@ -61,10 +61,21 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'stepupmountains',
+        'USER': 'stepupmountains',
+        'PASSWORD': 'elbrus',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+#}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
