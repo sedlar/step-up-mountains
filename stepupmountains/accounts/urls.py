014c47a9b749e22d2f1a05907d74ad41bd973e7c
Skokan <sedlar@mail.muni.cz>
2015-03-04 19:24:30 +0100

    Using builtin view for logout

--- stepupmountains/accounts/urls.py
+++ stepupmountains/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
-
 from stepupmountains.accounts import views
 import django.contrib.auth.views
+from django.contrib.auth.forms import UserCreationForm
 
 APEND_SLASH = True
 urlpatterns = patterns(
 	'',
-	url(r'login/$', 'django.contrib.auth.views.login', {'template_name': 'stepupmountains/login.html'}),
-	url(r'logout/$', views.auth_logout, name='auth_logout'),
+	url(r'login/$', 'django.contrib.auth.views.login', {'template_name': 'stepupmountains/login.html'}, name='login'),
+	url(r'logout/$', 'django.contrib.auth.views.logout', {'next_page': 'stepupmountains:mountain_list'}),
+	#url(r'register/$', 'django.contrib.auth.views.login', {'authentication_form': UserCreationForm, 'template_name': 'stepupmountains/register.html'}, name='register'),
 	)
