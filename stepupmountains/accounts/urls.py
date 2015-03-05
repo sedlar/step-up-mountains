60ca9930d950d62574108c173ce8b0f81289c537
Skokan <sedlar@mail.muni.cz>
2015-03-05 21:26:20 +0100

    Added register and password change form

--- stepupmountains/accounts/urls.py
+++ stepupmountains/accounts/urls.py
@@ -2,11 +2,21 @@ from django.conf.urls import patterns, include, url
 from stepupmountains.accounts import views
 import django.contrib.auth.views
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.core.urlresolvers import reverse
 
 APEND_SLASH = True
 urlpatterns = patterns(
-	'',
-	url(r'login/$', 'django.contrib.auth.views.login', {'template_name': 'stepupmountains/login.html'}, name='login'),
-	url(r'logout/$', 'django.contrib.auth.views.logout', {'next_page': 'stepupmountains:mountain_list'}),
-	#url(r'register/$', 'django.contrib.auth.views.login', {'authentication_form': UserCreationForm, 'template_name': 'stepupmountains/register.html'}, name='register'),
-	)
+    '',
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'stepupmountains/login.html'}, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': 'stepupmountains:mountain_list'}),
+    url(r'^change-password/$', 'django.contrib.auth.views.password_change', {'template_name': 'stepupmountains/changepassword.html', 'post_change_redirect': 'stepupmountains:accounts:change_password_done'}
+        , name='change_password'),
+    url(r'^change-password-done/$', 'django.contrib.auth.views.password_change_done', {'template_name': 'stepupmountains/changepassworddone.html'},
+        name='change_password_done'),
+    url(r'^register/$', CreateView.as_view(
+            template_name='stepupmountains/register.html',
+            form_class=UserCreationForm,
+            success_url='/accounts/login/')
+        , name='register'),
+    )
